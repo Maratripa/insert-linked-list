7803693af57a62858ee733b0cf2e9d14a8af18e0
Maratripa <maratripa@gmail.com>
2023-01-19 16:13:25 -0300

    Code structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    cell::RefCell,
+    rc::Rc
+};
+
+struct Node<T> {
+    next: Option<Rc<RefCell<Node<T>>>>,
+    pub value: Option<T>
+}
+
+impl<T> Node<T> {
+    fn new(value: T) -> Node<T> {
+        Node {
+            next: None,
+            value: Some(value)
+        }
+    }
+}
+
+struct SLL<T> {
+    head: Option<Rc<RefCell<Node<T>>>>,
+    len: usize
+}
+
+impl<T> SLL<T> {
+    pub fn new() -> SLL<T> {
+        SLL {
+            head: None,
+            len: 0
+        }
+    }
+
+    pub fn add(&mut self, value: T) {
+
+    }
+
+    pub fn append(&mut self, value: T) {
+
+    }
+
+    pub fn insert(&mut self, value: T, position: usize) {
+
+    }
+
+    pub fn remove(&mut self, position: usize) {
+        
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
