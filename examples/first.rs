e44e4735b3bc8b86ab1fc3d6839f6877bb6cf7e1
Maratripa <maratripa@gmail.com>
2023-01-19 21:33:24 -0300

    Examples

---
+++ examples/first.rs
@@ -0,0 +1,20 @@
+use linked_list::LinkedList;
+
+fn main() {
+    let mut ll: LinkedList<u8> = LinkedList::new();
+
+    ll.add(5);
+    ll.append(8);
+    ll.add(2);
+
+    let mut counter = 0;
+
+    for elem in ll.iter() {
+        if counter != 0 {
+            print!(" -> ");
+        }
+        print!("{}", elem);
+        counter = counter + 1;
+    }
+    print!("\n");
+}
